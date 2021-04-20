56f5e4d47b7e00ac5e8a52dd3a122380cac23c1b
camouflaged-penguin <82024665+camouflaged-penguin@users.noreply.github.com>
2021-04-20 15:44:10 +0200

    added background and basket

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -6,8 +6,9 @@ class SceneMain extends Phaser.Scene {
   {
     //load images or sounds
     //this.load.image("key","path")
+    this.load.image('background', 'assets/background.png');
     this.load.image('banana', 'assets/banana.png')
-    this.load.image('sky', 'assets/sky.png');
+    this.load.image('basket', 'assets/basket.png')
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
@@ -17,8 +18,9 @@ class SceneMain extends Phaser.Scene {
   }
   create() {
     console.log("message");
-      this.sky = this.add.image(400, 300, 'sky');
-      this.banana = this.add.image(200,250, 'banana');
+      this.background = this.add.image(400, 300, 'background');
+    //  this.banana = this.add.image(200, 250, 'banana');
+      this.basket = this.add.image(200, 250, 'basket')
   }
   update() {
     // everything that is constantly checked
