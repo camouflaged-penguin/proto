d39c4f588041d0a467d1a47ab5dfdc348f4b9567
camouflaged-penguin <82024665+camouflaged-penguin@users.noreply.github.com>
2021-04-22 12:46:52 +0200

    collide world

--- sceneMain.js
+++ sceneMain.js
@@ -18,34 +18,22 @@ class SceneMain extends Phaser.Scene {
      { frameWidth: 32, frameHeight: 48 });
   }
   create() {
-      //wold bounds
-      //this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
-
-      this.background = this.add.image(0, 0, 'background');
-      this.background.setOrigin(0,0);
-
-      var bananaGroup = this.physics.add.group({
-        key: 'banana',
-        repeat: 1,
-        setXY: { x: (Phaser.Math.Between(150, 800)), y: 0, stepX: 70 }
-      });
-
+      console.log("message");
+      this.background = this.add.image(400, 300, 'background');
     //  this.background = this.add.image(400, 300, 'background');
     //  this.banana = this.add.image(200, 250, 'banana');
-      //player = this.physics.add.sprite(100, 450, 'dude');
-      //this.basket = this.add.image(200, 650, 'basket').setInteractive();
-      this.basket = this.physics.add.image(200, 650, 'basket').setInteractive();
+      this.basket = this.add.image(200, 250, 'basket').setInteractive();
       this.basket.scaleX=.5;
       this.basket.scaleY=.5;
-      this.basket.setCollideWorldBounds(true);
+
       this.input.setDraggable(this.basket);
 
       //  The pointer has to move 16 pixels before it's considered as a drag
       this.input.dragDistanceThreshold = 16;
+
       this.input.on('dragstart', function (pointer, gameObject) {
 
          //gameObject.setTint(0xff0000);
-         gameObject.alpha=.5;
 
      });
 
@@ -59,11 +47,8 @@ class SceneMain extends Phaser.Scene {
       this.input.on('dragend', function (pointer, gameObject) {
 
          //gameObject.clearTint();
-         gameObject.alpha=1;
 
      });
-
-      this.physics.add.collider(bananaGroup, this.basket, this.hitBasket);
 
 
 
@@ -72,30 +57,4 @@ class SceneMain extends Phaser.Scene {
     // everything that is constantly checked
     //kommentar hier
   }
-
-    hitBasket()
-    {
-
-    this.physics.pause();
-
-    this.basket.setTint(0xff0000);
-    console.log("stop")
-
-
-    };
-
-
-
-
 }
-
-// function hitBomb (player, bomb)
-// {
-//   this.physics.pause();
-//
-//   player.setTint(0xff0000);
-//
-//   player.anims.play('turn');
-//
-//   gameOver = true;
-// }
